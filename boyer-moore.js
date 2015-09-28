80f3d2d25b80a7c5a8c72c5befdb2ee0f328d2cd
Devin Nusbaum <d.w.nusbaum@gmail.com>
2015-09-27 22:49:40 -0400

    Make boyer-moore.js a CommonJS module

--- boyer-moore.js
+++ boyer-moore.js
@@ -1,3 +1,30 @@
+function search(needle, haystack) {
+    var badCharTable = makeBadCharTable(needle);
+    var goodSuffixTable = makeGoodSuffixTable(needle);
+
+    var haystackIndex = needle.length - 1;
+    var previousHaystackIndex = -1;
+    var needleIndex = needle.length - 1;
+
+
+    while (haystackIndex < haystack.length) {
+        if (needleIndex < 0 || haystackIndex == previousHaystackIndex) {
+            return true;
+        } else if (needle.charAt(needleIndex) === haystack.charAt(haystackIndex)) {
+            haystackIndex--;
+            needleIndex--;
+        } else {
+            var shift = Math.max(badCharTable(haystack.charAt(haystackIndex)), goodSuffixTable[needleIndex]);
+            previousHaystackIndex = shift >= needleIndex + 1 ? haystackIndex : previousHaystackIndex;
+            haystackIndex += shift;
+            needleIndex = needle.length - 1;
+        }
+    }
+
+    // No match found, return false
+    return false;
+}
+
 function makeBadCharTable(needle) {
     var table = [];
     for (var i = 0; i < needle.length; i++) {
@@ -48,38 +75,9 @@ function getSuffixLength(needle, index) {
     return suffixLength;
 }
 
-function search(needle, haystack) {
-    var badCharTable = makeBadCharTable(needle);
-    var goodSuffixTable = makeGoodSuffixTable(needle);
-
-    var haystackIndex = needle.length - 1;
-    var previousHaystackIndex = -1;
-    var needleIndex = needle.length - 1;
-
-
-    while (haystackIndex < haystack.length) {
-        if (needleIndex < 0 || haystackIndex == previousHaystackIndex) {
-            return true;
-        } else if (needle.charAt(needleIndex) === haystack.charAt(haystackIndex)) {
-            haystackIndex--;
-            needleIndex--;
-        } else {
-            var shift = Math.max(badCharTable(haystack.charAt(haystackIndex)), goodSuffixTable[needleIndex]);
-            previousHaystackIndex = shift >= needleIndex + 1 ? haystackIndex : previousHaystackIndex;
-            haystackIndex += shift;
-            needleIndex = needle.length - 1;
-        }
-    }
-
-    // No match found, return false
-    return false;
-}
-
-console.log(search("computer", "computer"));
-console.log(search("muter", "computer"));
-console.log(search("puter", "computer"));
-console.log(search("", "computer"));
-console.log(search("example", "here is a simple example"));
-console.log(search("zaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
-console.log(makeGoodSuffixTable("abcxxxabc"));
-console.log(makeGoodSuffixTable("abyxcdeyx"));
+module.exports = {
+    search: search,
+    _makeBadCharTable: makeBadCharTable,
+    _makeGoodSuffixTable: makeGoodSuffixTable,
+    _getSuffixLength: getSuffixLength
+};
