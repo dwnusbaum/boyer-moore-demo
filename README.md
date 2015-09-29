95b012678adac94f662fe498522e46d9ac70cbaf
Devin Nusbaum <d.w.nusbaum@gmail.com>
2015-09-28 22:11:18 -0400

    Rename boyer-moore.js to boyerMoore.js

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-## boyer-moore.js
+## boyerMoore.js
 
-Implementation of the Boyer Moore string search algorithm.  [Here](https://dwnusbaum.github.io/boyer-moore/index.html) is an interactive demo of how the algorithm works.
+Implementation of the Boyer-Moore fast string search algorithm.  [Here](https://dwnusbaum.github.io/boyerMoore.js/index.html) is an interactive demo of how the algorithm works.
--- spec/boyerMooreSpec.js
+++ spec/boyerMooreSpec.js
@@ -1,51 +1,51 @@
-describe("boyer-moore.js", function() {
-    var bm = require("../boyer-moore.js");
+describe("boyerMoore.js", function() {
+    var boyerMoore = require("../boyerMoore.js");
 
-    describe("boyer-moore.js exports", function() {
+    describe("boyerMoore.js exports", function() {
         it("exports a function called search", function() {
-            expect(bm.search).toBeDefined();
+            expect(boyerMoore.search).toBeDefined();
         });
 
         it("exports a function called _makeBadCharTable", function() {
-            expect(bm._makeBadCharTable).toBeDefined();
+            expect(boyerMoore._makeBadCharTable).toBeDefined();
         });
 
         it("exports a function called _makeGoodSuffixTable", function() {
-            expect(bm._makeGoodSuffixTable).toBeDefined();
+            expect(boyerMoore._makeGoodSuffixTable).toBeDefined();
         });
 
         it("exports a function called _getSuffixLength", function() {
-            expect(bm._makeGoodSuffixTable).toBeDefined();
+            expect(boyerMoore._makeGoodSuffixTable).toBeDefined();
         });
     });
 
-    describe("boyer-moore.js public api", function() {
-        describe("boyer-moore.search", function() {
+    describe("boyerMoore.js public api", function() {
+        describe("boyerMoore.search", function() {
             it("finds \"computer\" in \"computer\"", function() {
-                expect(bm.search("computer", "computer")).toBe(true);
+                expect(boyerMoore.search("computer", "computer")).toBe(true);
             });
 
             it("find \"puter\" in \"computer\"", function() {
-                expect(bm.search("puter", "computer")).toBe(true);
+                expect(boyerMoore.search("puter", "computer")).toBe(true);
             });
 
             it("finds the empty string in any string", function() {
-                expect(bm.search("", "computer")).toBe(true);
-                expect(bm.search("", "")).toBe(true);
+                expect(boyerMoore.search("", "computer")).toBe(true);
+                expect(boyerMoore.search("", "")).toBe(true);
             });
 
             it("does not find \"muter\" in \"computer\"", function() {
-                expect(bm.search("muter", "computer")).toBe(false);
+                expect(boyerMoore.search("muter", "computer")).toBe(false);
             });
 
             it("does not find a ('z' followed by 'a's) in (a string of all 'a's)", function() {
-                expect(bm.search("zaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")).toBe(false);
+                expect(boyerMoore.search("zaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")).toBe(false);
             });
         });
     });
 
-    describe("boyer-moore.js private api", function() {
-        describe("_makeBadCharTable", function() {
+    describe("boyerMoore.js private api", function() {
+        describe("boyerMoore._makeBadCharTable", function() {
             it ("creates the proper table for \"abcd\"", function() {
                 var correctTable = {
                     a: 3,
@@ -53,7 +53,7 @@ describe("boyer-moore.js", function() {
                     c: 1,
                     d: 0,
                 };
-                var table = bm._makeBadCharTable("abcd");
+                var table = boyerMoore._makeBadCharTable("abcd");
                 expect(table("a")).toEqual(correctTable["a"]);
                 expect(table("b")).toEqual(correctTable["b"]);
                 expect(table("c")).toEqual(correctTable["c"]);
@@ -62,15 +62,15 @@ describe("boyer-moore.js", function() {
             });
         });
 
-        describe("_makeGoodSuffixTable", function() {
+        describe("boyerMoore._makeGoodSuffixTable", function() {
             it("creates the proper table for \"abcxxxabc\"", function() {
                 var correctTable = [14, 13, 12, 11, 10, 9, 11, 10, 1];
-                expect(bm._makeGoodSuffixTable("abcxxxabc")).toEqual(correctTable);
+                expect(boyerMoore._makeGoodSuffixTable("abcxxxabc")).toEqual(correctTable);
             });
 
             it("creates the proper table for \"abyxcdeyx\"", function() {
                 var correctTable = [17, 16, 15, 14, 13, 12, 7, 10, 1];
-                expect(bm._makeGoodSuffixTable("abyxcdeyx")).toEqual(correctTable);
+                expect(boyerMoore._makeGoodSuffixTable("abyxcdeyx")).toEqual(correctTable);
             });
         });
     });
