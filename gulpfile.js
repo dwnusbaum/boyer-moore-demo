ec0d0c1bd65fa68ba98754385132decb5901a6e3
Devin Nusbaum <d.w.nusbaum@gmail.com>
2015-09-30 16:25:35 -0400

    Use browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,38 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var babel = require('gulp-babel');
+var browserify = require('browserify');
 var connect = require('gulp-connect');
+var source = require('vinyl-source-stream');
 
-gulp.task('connect', function() {
+gulp.task('connect', ['browserify'], function() {
   connect.server({
     root: 'app',
     livereload: true
   });
 });
 
-gulp.task('html', function () {
+gulp.task('html', ['browserify'], function () {
   gulp.src('./app/*.html')
     .pipe(connect.reload());
 });
 
-gulp.task('babel', function () {
-  gulp.src('./app/js/*.js')
-    .pipe(babel())
-      .on('error', gutil.log)
+gulp.task('browserify', ['babel'], function() {
+  return browserify('./app/build/translated/demo.js')
+    .bundle()
+    .pipe(source('bundle.js'))
     .pipe(gulp.dest('./app/build/'));
 });
 
-gulp.task('watch', function () {
-  gulp.watch(['./app/js/*.js'], ['babel', 'html']);
-  gulp.watch(['./app/*.html', './app/styles/*.css'], ['html']);
+gulp.task('babel', function () {
+  return gulp.src('./app/js/*.js')
+    .pipe(babel())
+      .on('error', gutil.log)
+    .pipe(gulp.dest('./app/build/translated'));
 });
 
-gulp.task('default', ['babel', 'connect', 'watch']);
+gulp.task('watch', function () {
+  gulp.watch(['./app/*.html', './app/styles/*.css', './app/js/*.js'], ['html']);
+});
+
+gulp.task('default', ['connect', 'watch']);
