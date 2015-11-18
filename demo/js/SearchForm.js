7c056bb98af8461b2fd91515ef7910b82a790fd6
Devin Nusbaum <d.w.nusbaum@gmail.com>
2015-11-17 21:19:28 -0500

    Make import naming consistent

--- demo/js/SearchForm.js
+++ demo/js/SearchForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import BoyerMoore from "./BoyerMooreDemo.js";
+import BoyerMoore from "./BoyerMooreDemo";
 
 class SearchForm extends React.Component {
     constructor() {
