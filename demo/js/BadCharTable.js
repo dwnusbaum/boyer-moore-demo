7466222d755720ba9d1e687455830d0b52b9a6f4
Devin Nusbaum <d.w.nusbaum@gmail.com>
2015-11-17 21:24:54 -0500

    Refactor BadCharTable into a stateless functional component

--- demo/js/BadCharTable.js
+++ demo/js/BadCharTable.js
@@ -1,4 +1,4 @@
-const React = require("react");
+import React from "react";
 
 const highlightActions = new Set([
     "COMPARE_NOT_EQUAL",
@@ -10,67 +10,60 @@ const highlightStyle = {
     color: "blue",
 }
 
-const BadCharTable = React.createClass({
-    render: function() {
-        const badCharTable = this.props.badCharTable;
-        const action = this.props.action;
-        const haystack = this.props.haystack;
-        const needle = this.props.needle;
+let BadCharTable = ({badCharTable, action, haystack, needle}) => {
+    let needleSet = new Set(needle.split(""));
+    // Add a dummy element that is not in the needle array to show the shift
+    // amount for characters not in the needle.
+    needleSet.add("other");
 
-        let needleSet = new Set(needle.split(""));
-        // Add a dummy element definitely not in the needle array to show the
-        // shift amount for characters not in the needle.
-        needleSet.add("other");
-
-        let highlightChar = "";
-        if (highlightActions.has(action.name)) {
-            const haystackChar = haystack.charAt(action.haystackIndex);
-            if (needleSet.has(haystackChar)) {
-                highlightChar = haystackChar;
-            } else {
-                highlightChar = "other";
-            }
+    let highlightChar = "";
+    if (highlightActions.has(action.name)) {
+        const haystackChar = haystack.charAt(action.haystackIndex);
+        if (needleSet.has(haystackChar)) {
+            highlightChar = haystackChar;
+        } else {
+            highlightChar = "other";
         }
-
-        let tableHeader = [];
-        needleSet.forEach(function(char, index) {
-            tableHeader.push(
-                <td key={index}>
-                    <span style={char === highlightChar ? highlightStyle : {}}>
-                        {char}
-                    </span>
-                </td>
-            );
-        });
-
-        let tableBody = [];
-        needleSet.forEach(function(char, index) {
-            tableBody.push(
-                <td key={index}>
-                    <span style={char === highlightChar ? highlightStyle : {}}>
-                        {badCharTable(char)}
-                    </span>
-                </td>
-            );
-        });
-
-        return (
-            <div>
-                <table className="shiftTable">
-                    <thead>
-                        <tr>
-                            {tableHeader}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            {tableBody}
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        );
     }
-});
 
-module.exports = BadCharTable;
+    let tableHeader = [];
+    needleSet.forEach(function(char, index) {
+        tableHeader.push(
+            <td key={index}>
+                <span style={char === highlightChar ? highlightStyle : {}}>
+                    {char}
+                </span>
+            </td>
+        );
+    });
+
+    let tableBody = [];
+    needleSet.forEach(function(char, index) {
+        tableBody.push(
+            <td key={index}>
+                <span style={char === highlightChar ? highlightStyle : {}}>
+                    {badCharTable(char)}
+                </span>
+            </td>
+        );
+    });
+
+    return (
+        <div>
+            <table className="shiftTable">
+                <thead>
+                    <tr>
+                        {tableHeader}
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        {tableBody}
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default BadCharTable;
