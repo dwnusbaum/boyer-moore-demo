a3434dc2faad3bc0acd0d50efb17e910a9019690
Devin Nusbaum <d.w.nusbaum@gmail.com>
2015-11-18 12:27:18 -0500

    Clarify comment about centering the bad character table

--- demo/js/BadCharModal.js
+++ demo/js/BadCharModal.js
@@ -65,7 +65,8 @@ class BadCharModal extends React.Component {
                                 For example, if the pattern is "example" then the bad
                                 character table is:
                             </p>
-                            <div className="text-center"> {/* Text-center will center the table */}
+                        {/* Text-center will center the table because it is an inline-block element */}
+                            <div className="text-center">
                                 <BadCharTable
                                     badCharTable={BoyerMoore.makeBadCharTable(needle)}
                                     action={{name: undefined}}
